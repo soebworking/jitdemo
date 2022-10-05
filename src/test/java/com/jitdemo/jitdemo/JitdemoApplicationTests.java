933551bebeb65c6c6b0903ef0e20b882d07d6c19
Mohamedsoeb Ahmednashirwala <soeb.working@gmail.com>
2022-10-05 10:03:33 +0530

    project structure created

---
+++ src/test/java/com/jitdemo/jitdemo/JitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitdemo.jitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JitdemoApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
