00c80b0704864b7f19a2f65b6305d0fa26fb3bcc
Mohamedsoeb Ahmednashirwala <soeb.working@gmail.com>
2022-10-07 22:07:05 +0530

    -Intrim commit

--- src/main/java/com/jitdemo/jitdemo/service/LocationService.java
+++ src/main/java/com/jitdemo/jitdemo/service/LocationService.java
@@ -9,6 +9,7 @@ import javax.transaction.Transactional;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -24,19 +25,14 @@ public class LocationService {
 
 
     public String getUserLocationsFromDates(UUID uuid, String fromDate, String toDate) throws ParseException {
-        System.out.println("Inside Service");
 
         //first check with date is lower
         Date fromDateSql = null;
         Date toDateSql = null;
-
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date date1 = sdf.parse(fromDate);
         Date date2 = sdf.parse(toDate);
-
-
         int result = date1.compareTo(date2);
-
         //if fromDate < toDate then no change/swap needed
         fromDateSql = sdf.parse(fromDate);
         toDateSql = sdf.parse(toDate);
@@ -45,9 +41,8 @@ public class LocationService {
             fromDateSql = sdf.parse(toDate);
             toDateSql = sdf.parse(fromDate);
         }
-        System.out.println("From date : " + fromDateSql);
-        System.out.println("To date : " + toDateSql);
-        locationRepository.getAllBetweenDates(uuid, fromDateSql, toDateSql);
+
+        List<Locations> locationResults =  locationRepository.getAllBetweenDates(uuid, fromDateSql, toDateSql);
         return null;
     }
 }
