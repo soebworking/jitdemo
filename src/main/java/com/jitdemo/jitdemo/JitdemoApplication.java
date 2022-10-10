2240a97f6e1b1560db4698a26f3eb490d91e18e9
Mohamedsoeb Ahmednashirwala <soeb.working@gmail.com>
2022-10-10 09:51:55 +0530

    - Removed Hystrix, unintended added.

--- src/main/java/com/jitdemo/jitdemo/JitdemoApplication.java
+++ src/main/java/com/jitdemo/jitdemo/JitdemoApplication.java
@@ -2,10 +2,8 @@ package com.jitdemo.jitdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableHystrix
 public class JitdemoApplication {
 
 	public static void main(String[] args) {
